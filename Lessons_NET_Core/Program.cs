10223d322c002caf0a230d37e5f2563ee8d3be19
Peter <vorobey.petr@gmail.com>
2024-08-11 19:56:53 +0300

    Add project files.

---
+++ Lessons_NET_Core/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+namespace Lessons_NER_Core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //LESSON 39.ИНДЕКСЫ И ДИАПАЗОНЫ В C#
+
+            int[] myArray = { 2, 3, 4, 5 };
+
+            Console.WriteLine(myArray[^1]); // такой знак - ^1 - он будет читать наш массив сконца, первый элемент с конца, а 0 с начало 
+
+
+
+
+
+
+
+
+
+        }
+    
+
+    }
+
+}  
+  
