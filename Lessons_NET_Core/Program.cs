690078c1c09e7bd44c8eb82986572bee482cc111
Peter <vorobey.petr@gmail.com>
2024-08-11 21:22:09 +0300

    LESSON 39.ИНДЕКСЫ И ДИАПАЗОНЫ В C#

--- Lessons_NET_Core/Program.cs
+++ Lessons_NET_Core/Program.cs
@@ -11,7 +11,7 @@ namespace Lessons_NER_Core
 
             /*
             int[] myArray = { 2, 3, 4, 5 }; // Элементы в массиве
-            int[] myArray2 = myArray[1..4]; // Будет извлекать элементы с 1 по 4, можно написат ьи [..4]
+            int[] myArray2 = myArray[1..4]; // Будет извлекать элементы с 1 по 4, можно написать и [..4]
                                             // Можно написать так от 5 элемента и до конца [5..]
             Console.WriteLine(myArray[^1]); // такой знак - ^1 - он будет извлекать наш массив с конца, первый элемент с конца, а 0 с начало 
                                             // ^ - это структура
@@ -22,7 +22,7 @@ namespace Lessons_NER_Core
             int[] myArray = { 2, 3, 4, 5 };
             Index myIndex = ^1; // Мы создаем индекс куда помещаем ^1 
 
-            Console.WriteLine(myArray[myIndex]); // Сразу покажет жлемент последний из массива, с конца
+            Console.WriteLine(myArray[myIndex]); // Сразу покажет элемент последний из массива, с конца
             Console.WriteLine();
             Console.WriteLine($"value  {myIndex.Value} isFromEnd {myIndex.IsFromEnd}");
             */
@@ -47,14 +47,14 @@ namespace Lessons_NER_Core
 
             //Example: Работы со строками:
 
-
+            /*
             string str = "Hello World";
 
             Console.WriteLine(str[^2..]); // Если в [0] укажем индекс, то он нам выведит первый символ H
                                           // Можно указывать [^1..] - с конца будет d, если написать [^2..] - то будет ld
                                           // Можно так [0..5]
 
-
+            */
 
 
 
